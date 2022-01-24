cee346408746942d51365610a66943bf92bc7655
biggvladek <zuevvlad55@gmail.com>
2022-01-24 23:41:37 +0300

    moduls

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+use minigrep::Config;
 
 
 fn main(){
@@ -13,7 +12,7 @@ fn main(){
     println!("Searching for {}",config.query);
     println!("In file {}",config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error : {}",e);
 
         process::exit(1);
@@ -21,27 +20,3 @@ fn main(){
 
 }
 
-fn run(config:Config) -> Result<(),Box<dyn Error>>{
-    let contens = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}",contens);
-    Ok(())
-
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config,&'static str> {
-        if args.len() <3 {
-            return Err("not enough arguments!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
-
-    }
-
-}
