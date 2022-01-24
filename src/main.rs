45b40c20f0893dd69515aeea0d8c3ef9200150ef
biggvladek <zuevvlad55@gmail.com>
2022-01-24 21:19:19 +0300

    2 commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {}",query);
+    println!("In file {}",filename);
+
+    let contens = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}",contens);
+
 }
