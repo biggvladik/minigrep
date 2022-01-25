ee5bfe56873e10c9238b3348268176356ad1c20d
biggvladek <zuevvlad55@gmail.com>
2022-01-25 21:40:01 +0300

    finish commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
  pub fn run(config:Config) -> Result<(),Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
+
+     let results = if config.case_sensitive {
+         search(&config.query,&contents)
+     } else {
+        search_case_insensitive(&config.query,&contents)
+     };
+
      for line in search(&config.query, &contents) {
          println!("{}",line);
      }
@@ -12,6 +20,7 @@ use std::error::Error;
  pub struct Config {
     pub query: String,
     pub filename: String,
+     pub  case_sensitive: bool,
 }
 
 impl Config {
@@ -21,8 +30,9 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config {query, filename})
+        Ok(Config {query, filename,case_sensitive})
 
     }
 
@@ -38,6 +48,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }   results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,5 +75,18 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:","Trust me."],
+            search_case_insensitive(query,contents)
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use minigrep::Config;
 fn main(){
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
